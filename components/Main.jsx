aa85b9fe84d5706690b50a5628a1a0c3f985acca
Anjali2201 <anjalikushwaha031@gmail.com>
2023-09-10 00:20:26 +0530

    landing page

--- components/Main.jsx
+++ components/Main.jsx
@@ -15,14 +15,18 @@ const Main = () => {
           <h1 className="py-4 text-gray-700">
             Hi, I&#39;m <span className="text-[#5651e5]"> Anjali</span>
           </h1>
-          <h1 className="py-2 text-gray-700">A Front-End Web Developer</h1>
+          <h1 className="py-2 text-gray-700">Data Science | Web Developer</h1>
           <p className="py-4 text-gray-600 sm:max-w-[70%] m-auto">
             I’m focused on building responsive front-end web applications
-            integrating back-end technologies.
+            integrating back-end technologies. Along with that, I am also a Data
+            Science enthusiast.
           </p>
+
+          {/* --------------------------- Social media icons ------------------------- */}
           <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            {/* --- Linkedin------ */}
             <a
-              href="https://www.linkedin.com/in/clint-briley-50056920a/"
+              href="https://www.linkedin.com/in/anjallliii/"
               target="_blank"
               rel="noreferrer"
             >
@@ -30,8 +34,10 @@ const Main = () => {
                 <FaLinkedinIn />
               </div>
             </a>
+
+            {/* --- Github------ */}
             <a
-              href="https://github.com/fireclint"
+              href="https://github.com/Anjali2201"
               target="_blank"
               rel="noreferrer"
             >
@@ -39,11 +45,15 @@ const Main = () => {
                 <FaGithub />
               </div>
             </a>
+
+            {/* --- Email------ */}
             <Link href="/#contact">
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <AiOutlineMail />
               </div>
             </Link>
+
+            {/* --- Resume------ */}
             <Link href="/resume">
               <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
                 <BsFillPersonLinesFill />
@@ -52,9 +62,6 @@ const Main = () => {
           </div>
         </div>
       </div>
-
-
-      
     </div>
   );
 };
